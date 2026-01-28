c20ea2d90fbfc74fb54d50caaa67e6be57260a2e
Iyed Aloui <lisette9.a@gmail.com>
2026-01-28 22:48:20 +0100

    fix next2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:10000/api" : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
