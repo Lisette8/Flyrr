9c01e091fe6aadd1b4f3628cd1f98eababb6ffdc
Iyed Aloui <lisette9.a@gmail.com>
2026-01-28 21:53:11 +0100

    deployment fix 3

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : '/';
+const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : "https://flyrr.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
